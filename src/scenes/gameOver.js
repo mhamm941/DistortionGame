9c588bcff88d451a0fc8cfc79566e726cb5c2bf3
Amber <aayanai1224@gmail.com>
2020-05-17 22:12:10 -0700

    TYPO

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -19,7 +19,7 @@ class gameOver extends Phaser.Scene {
         this.add.text(centerX, centerY + 64, "This scene will be used for game overs",{
             fontSize: '20px'
         })
-        this.add.text(centerX, centerY + 96, "Press ESC to go back to game or SPACE to go to menu",{
+        this.add.text(centerX, centerY + 96, "Press ENTER to go back to game or SPACE to go to menu",{
             fontSize: '20px'
         })
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
